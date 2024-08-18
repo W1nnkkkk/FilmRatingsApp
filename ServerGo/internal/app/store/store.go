7b34f9c2d8e93f19c1c6ca4f814d72d5668e1a97
Winnkkkk <tivchenko5442@gmail.com>
2024-08-18 13:12:36 +0300

    Update image policy now binary images!!!

--- ServerGo/internal/app/store/store.go
+++ ServerGo/internal/app/store/store.go
@@ -8,6 +8,7 @@ import (
 	"github.com/W1nnkkkk/FilmRatingsApp.git/internal/app/logger"
 	"github.com/W1nnkkkk/FilmRatingsApp.git/internal/app/model"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -33,32 +34,41 @@ func InitStore(host, database, collection string, port int, path string) (Store,
 }
 
 func (s *Store) FindMovie(filter bson.D) ([]model.Movie, error) {
-	results := []model.Movie{}
+    results := []model.Movie{}
 
-	cur, err := s.collection.Find(context.TODO(), filter)
+    cur, err := s.collection.Find(context.TODO(), filter)
+    if err != nil {
+        s.Logger.LogErrToFile(err)
+        return nil, err
+    }
+    defer cur.Close(context.TODO())
 
-	if err != nil {
-		s.Logger.LogErrToFile(err)
-		return nil, err
-	}
-	defer cur.Close(context.TODO())
+    for cur.Next(context.TODO()) {
+        var result model.Movie
+        if err := cur.Decode(&result); err != nil {
+            s.Logger.LogErrToFile(err)
+            return nil, err
+        }
 
-	for cur.Next(context.TODO()) {
-		var result model.Movie
-		if err := cur.Decode(&result); err != nil {
-			s.Logger.LogErrToFile(err)
-			return nil, err
-		}
-		results = append(results, result)
-	}
+		if imageBinary, ok := result.Image.(primitive.Binary); ok {
+            result.Image = imageBinary.Data
+        } else {
+            s.Logger.LogErrToFile(fmt.Errorf("Ошибка при извлечении данных изображения: %v", result.Image))
+        }
 
-	if err := cur.Err(); err != nil {
-		s.Logger.LogErrToFile(err)
-		return nil, err
-	}
 
-	return results, nil
+
+        results = append(results, result)
+    }
+
+    if err := cur.Err(); err != nil {
+        s.Logger.LogErrToFile(err)
+        return nil, err
+    }
+
+    return results, nil
 }
+
 
 func (s *Store) FindReview(filter bson.D) ([]model.Review, error) {
 	type ReviewResponse struct {
