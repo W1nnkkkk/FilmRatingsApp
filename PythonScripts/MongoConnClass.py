ebf4680da0a0ebd9a26bfaed0f09b94b333b0e69
Winnkkkk <tivchenko5442@gmail.com>
2024-08-20 17:30:11 +0300

    update script and image policy

---
+++ PythonScripts/MongoConnClass.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+
+class MongoEntity:
+    def __init__(self):
+        try:
+            self.conn = MongoClient("194.87.147.55", port=27017)
+            self.data_base = self.conn["moviebox"]
+            self.collection = self.data_base["movies"]
+        except Exception as err:
+            print(f"Error in mongodb connection {err}")
+
+    def add_to_collection(self, document):
+        try:
+            self.collection.insert_one(document=document)
+        except Exception as err:
+            print(f"Error in mongodb insert {err}")
--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -1,103 +1,141 @@
 # -- coding: utf-8 --
-
+import base64
+import time
+from concurrent.futures import ThreadPoolExecutor
 import requests
-import json
 from bs4 import BeautifulSoup
 import pandas as pd
-from translate import translate_key_for_json
-from concurrent.futures import ThreadPoolExecutor
-import os
+from fake_useragent import UserAgent
+from MongoConnClass import MongoEntity
 
-def merge_json_files(directory, output_filename):
-  data = []
-  for filename in os.listdir(directory):
-    if filename.endswith(".json"):
-      filepath = os.path.join(directory, filename)
-      with open(filepath, 'r', encoding='utf-8') as f:
-        try:
-          json_data = json.load(f)
-          data.append(json_data)
-        except json.JSONDecodeError:
-          print(f"Ошибка декодирования JSON: {filename}")
 
-  with open(output_filename, 'w', encoding='utf-8') as outfile:
-    json.dump(data, outfile, indent=4, ensure_ascii=False)
+main_url = "https://kino.mail.ru"
+extra_url = "/cinema/online"
+main_url_next_page = "?page="
+uri = []
+mongo_descriptor = MongoEntity()
+server = "http://127.0.0.1:7979"
+
 
 def main():
 
-    merge_json_files('data', 'movie.json')
+    # parse_site_to_films_uri(main_url + extra_url)
+    # for i in range(2, 119):
+    #     parse_site_to_films_uri(main_url + extra_url + main_url_next_page + str(i))
+    #
+    # create_exel_file('URL', uri)
 
-    # df = pd.read_excel('urls.xlsx')
-    # links = df['Url'].tolist()
-    # with ThreadPoolExecutor() as executor:
-    #     results = executor.map(parse_exel_data, links)
-    #     for result in results:
-    #         print(result)
-    # dFrame = pd.DataFrame(movies)
-    # dFrame.to_csv('movies.csv', index=False)
+    df = pd.read_excel('urls.xlsx')
+
+    links = df['URL'].tolist()
+
+    for link in links:
+        parse_table_data_and_insert_into_mongo(link)
 
 
-def parse_kinopoisk(url):
+def parse_table_data_and_insert_into_mongo(url):
     response = requests.get(url)
-    response.raise_for_status()  # Проверка на ошибки запроса
+    response.raise_for_status()
+
+    print(url)
 
     soup = BeautifulSoup(response.content, 'html.parser')
-    global movies
+    film_data = soup.find('div', class_='p-movie-info')
+    image = (soup.find("img", class_="picture__image picture__image_cover")["src"])
+    name = soup.find("h1", class_="text text_bold_giant color_white").text
+    upload_image(requests.get(image).content, server, name)
 
-    for film in soup.find_all('div', class_='shortpost'):
-        movie = {}
-        url = film.find('div', class_='postcover')
-        movie['Url'] = url.a['href']
-        
-        movies.append(movie)
+    try:
+        year = film_data.find('div', class_="margin_bottom_20").find('a').text
+    except Exception as err:
+        print(err)
+    duration = ""
+    try:
+        if film_data.find('span', class_="margin_left_40 nowrap").a:
+            duration = "Пока нет"
+        else:
+            if film_data.find('span', class_="margin_left_40 nowrap").text:
+                duration = film_data.find('span', class_="margin_left_40 nowrap").text
+    except Exception as err:
+        print(err)
 
-def create_excel(movies):
-    """Создает Excel-таблицу с информацией о фильмах."""
-    df = pd.DataFrame(movies)
+    genres = []
+    for genre in film_data.find_all('span', class_="badge__text"):
+        genres.append(genre.text)
+
+    country = ""
+    directors = []
+    starrings = []
+
+    try:
+        directors_a = (film_data.find('span', class_="p-truncate__inner js-toggle__truncate-inner")
+                       .find_all('a'))
+        for director in directors_a:
+            directors.append(director.text)
+
+        starring_a = (film_data.find('span', class_="p-truncate__inner js-toggle__truncate-inner")
+                      .findNext('span', class_="p-truncate__inner js-toggle__truncate-inner")
+                      .find_all('a'))
+        for starring in starring_a:
+            starrings.append(starring.text)
+
+        country_a = (film_data.find('span', class_="p-truncate__inner js-toggle__truncate-inner")
+                        .findNext('span', class_="p-truncate__inner js-toggle__truncate-inner")
+                        .findNext('span', class_="p-truncate__inner js-toggle__truncate-inner")
+                        .find_all('a'))
+
+        for countries in country_a:
+            country += countries.text + " "
+    except Exception as err:
+        print(err)
+
+    document = {
+        "image": server + "/image/" + name + ".jpg",
+        "name": name,
+        "originalName": "",
+        "year": year,
+        "country": country,
+        "director": directors,
+        "genre": genres,
+        "duration": duration,
+        "starring": starrings
+    }
+
+    print(document)
+    # mongo_descriptor.add_to_collection(document=document)
+
+
+def upload_image(image_data, server_url, filename):
+    files = {
+        "image": base64.b64encode(image_data).decode("utf-8"),
+        "filename": filename
+    }
+
+    upload_response = requests.post(f"{server_url}/upload", json=files)
+
+    if upload_response.status_code == 200:
+      print(f"Картинка '{filename}' успешно загружена.")
+    else:
+      print(f"Ошибка загрузки: {upload_response.text}")
+
+
+def parse_site_to_films_uri(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    global uri
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for film_data in soup.find_all('a',
+        class_="link link_inline link-holder link-holder_itemevent link-holder_itemevent_small"):
+        uri.append(main_url + film_data['href'])
+
+
+def create_exel_file(key, list_values):
+    df = pd.DataFrame(list_values, columns=[key])
     df.to_excel('urls.xlsx', index=False)
 
-def parse_exel_data(url):
-    response = requests.get(url)
-    response.raise_for_status() 
 
-    global movies
-    movies.clear()
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-    for film_data in soup.find_all('div', class_='postdata clearfix'):
-        movie = {}
-        image = film_data.find('div', class_='mobile_cover').img['data-src']
-        movie['image'] = 'https://nu.baksino.website' + image
-        
-        description = film_data.find('div', class_='info')
-        
-        table = description.find("table")
-        for row in table.find_all("tr"):
-            cells = row.find_all("td")
-            if len(cells) == 2:
-                key = cells[0].text.strip()
-                value = cells[1].text.strip()
-                if key == "Год":
-                    value = value.split("/")[0].strip()
-                elif '/' in value:
-                    value = list(map(str.strip, value.split('/')))
-                elif ', ' in value:
-                    value = list(map(str.strip, value.split(', ')))
-                elif key in ["Качество:", "Слоган:"]:
-                    continue
-                movie[translate_key_for_json(key[:len(key) - 1])] = value
-        
-        print(movie)
-        print('\n')
-        name_file = ""
-        if (type(movie['name']) == list):
-            name_file = movie['name'][0][:255]
-        else:
-            name_file = movie['name'][:255]
-        with open(('data/' + name_file + '.json'), "w") as f:
-            json.dump(movie, f, ensure_ascii=False, indent=4)
-        movies.append(movie)
-     
 if __name__ == "__main__":
     main()
 
